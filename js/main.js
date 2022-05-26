618e0e616100136aa3d8b4ee178d7f2555b5ae4c
BusuiocDaniel <104071764+BusuiocDaniel@users.noreply.github.com>
2022-05-26 10:25:16 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const scrollto = (el) => {
+    let header = select('#header')
+    let offset = header.offsetHeight
+
+    if (!header.classList.contains('header-scrolled')) {
+      offset -= 16
+    }
+
+    let elementPos = select(el).offsetTop
+    window.scrollTo({
+      top: elementPos - offset,
+      behavior: 'smooth'
+    })
+  }
